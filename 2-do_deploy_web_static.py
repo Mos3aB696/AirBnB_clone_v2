d2184ea3fcf16bbec2d590fc85c9f60af2128ef7
Mos3aB696 <mos3ab696@gmail.com>
2024-02-02 01:05:53 +0200

    Ta_3_sk

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -6,25 +6,32 @@ archive to the web servers
 
 from fabric.api import put, run, env
 from os.path import exists
+
+
 env.hosts = ['35.175.64.31', '52.3.244.199']
 
 
 def do_deploy(archive_path):
     """distributes an archive to the web servers"""
-    if exists(archive_path) is False:
+    if not exists(archive_path):
+        print("Archive does not exist.")
         return False
     try:
         file_n = archive_path.split("/")[-1]
         no_ext = file_n.split(".")[0]
         path = "/data/web_static/releases/"
         put(archive_path, '/tmp/')
-        run('mkdir -p {}{}/'.format(path, no_ext))
-        run('tar -xzf /tmp/{} -C {}{}/'.format(file_n, path, no_ext))
-        run('rm /tmp/{}'.format(file_n))
-        run('mv {0}{1}/web_static/* {0}{1}/'.format(path, no_ext))
-        run('rm -rf {}{}/web_static'.format(path, no_ext))
-        run('rm -rf /data/web_static/current')
-        run('ln -s {}{}/ /data/web_static/current'.format(path, no_ext))
+        run('sudo mkdir -p {}{}/'.format(path, no_ext))
+        run('sudo tar -xzf /tmp/{} -C {}{}/'.format(file_n, path, no_ext))
+        run('sudo rm /tmp/{}'.format(file_n))
+        run('sudo mv {0}{1}/web_static/* {0}{1}/'.format(path, no_ext))
+        run('sudo rm -rf {}{}/web_static'.format(path, no_ext))
+        run('sudo rm -rf /data/web_static/current')
+        run('sudo ln -s {}{}/ /data/web_static/current'.format(path, no_ext))
+        if run('test -e /data/web_static/current').failed:
+            print("Failed to create symlink.")
+            return False
         return True
-    except:
+    except Exception as e:
+        print("An error occurred during deployment: {}".format(str(e)))
         return False
