e0f01ea25bcc5e76faf3c7422cd9df3c05b1a241
Mos3aB696 <mos3ab696@gmail.com>
2024-02-01 23:03:58 +0200

    Ta_1_sk

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """
-Fabric script that generates a tgz archive from the contents of the web_static
-folder of the AirBnB Clone repo
+    script that generates a .tgz archive from the contents of the web_static
+    folder of your AirBnB Clone repo, using the function do_pack.
 """
 
 from datetime import datetime
